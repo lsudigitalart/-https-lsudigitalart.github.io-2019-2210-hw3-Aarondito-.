d678dae08c52904255bb9db45a40fd3971379603
Aarondito <54543739+Aarondito@users.noreply.github.com>
2019-09-17 13:55:03 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
-function setup(){
+function setup(){ // hopefully i can make some eyes bleed.
     createCanvas(3200, 750);
     background(255,252,0);
   
     for(let y = 10; y < 750; y += 15){
       for(let x = 10; x < 3200; x += 20){
-        stroke(102,255,0);
+        stroke(0,229,238);// blue line side
+        strokeWeight(4);
+        rect(x, y, 13,4 );
+        stroke(0,229,238); //blue line up
         strokeWeight(4);
         rect(x, y, 4,10 );
-        stroke(220,0,230);
-        strokeWeight(1);
+        stroke(255,0,0); //red box
+        strokeWeight(1.5);
         rect(x, y, 7,7 );
-        stroke('#fae');
-        strokeWeight(4);
+        stroke('#fae'); //dot
+        strokeWeight(3);
         fill(color(0, 0, 255));
         circle(x, y, 10);
 
